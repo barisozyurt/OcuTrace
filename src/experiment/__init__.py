19057c470b2a49d693c0ec48104090741e9744ae
Baris Ozyurt <barisozyurt@gmail.com>
2026-03-18 13:57:11 +0300

    feat: expose experiment package public API

---
+++ src/experiment/__init__.py
@@ -0,0 +1,19 @@
+"""Gap antisaccade experiment paradigm."""
+from src.experiment.paradigm import TrialSpec, generate_trial_sequence
+from src.experiment.stimulus import (
+    StimulusConfig,
+    TrialTimestamps,
+    create_stimulus_config,
+    compute_trial_frame_counts,
+    run_single_trial,
+)
+
+__all__ = [
+    "TrialSpec",
+    "generate_trial_sequence",
+    "StimulusConfig",
+    "TrialTimestamps",
+    "create_stimulus_config",
+    "compute_trial_frame_counts",
+    "run_single_trial",
+]
